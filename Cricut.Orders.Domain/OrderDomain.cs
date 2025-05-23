556c5ec818e4c89fad809895d594c36393023378
Aaron Johnson <aaronj84@gmail.com>
2025-05-23 12:16:59 -0600

    Got the initial methods created and confirmed i can call it

--- Cricut.Orders.Domain/OrderDomain.cs
+++ Cricut.Orders.Domain/OrderDomain.cs
@@ -1,10 +1,12 @@
 ﻿using Cricut.Orders.Domain.Models;
+using System.Diagnostics;
 
 namespace Cricut.Orders.Domain
 {
     public interface IOrderDomain
     {
         Task<Order> CreateNewOrderAsync(Order order);
+        Task<IEnumerable<Order>> GetOrdersByCustomerId(int custID);
     }
 
     public class OrderDomain : IOrderDomain
@@ -20,6 +22,13 @@ namespace Cricut.Orders.Domain
         {
             var updatedOrder = await _orderStore.SaveOrderAsync(order);
             return updatedOrder;
+        }
+
+        //GenAI Note: Used ChatGTP to suggest the correct return type. Didn't use Task<ActionResult<IEnum... to avoid further imports for purpose of simplicity in the challenge.
+        public async Task<IEnumerable<Order>> GetOrdersByCustomerId(int custID)
+        {
+            Debug.Print("tsup yo");
+            return null;
         }
     }
 }
